0ac14bb03a9a40f7536cacb2d634a96ea386a231
yousetsu <sarujincanon@gmail.com>
2026-02-14 12:23:32 +0900

    dockerのpostgreに接続するように切り替え

--- src/main/java/com/example/todoapp/service/TodoService.java
+++ src/main/java/com/example/todoapp/service/TodoService.java
@@ -3,6 +3,10 @@ package com.example.todoapp.service;
 import com.example.todoapp.domain.Todo;
 import com.example.todoapp.dto.TodoCreateRequest;
 import com.example.todoapp.dto.TodoResponse;
+// postgre ins start
+import com.example.todoapp.dto.TodoUpdateRequest;
+import org.springframework.transaction.annotation.Transactional;
+// postgre ins end
 import com.example.todoapp.exception.ResourceNotFoundException;
 import com.example.todoapp.repository.TodoRepository;
 import org.springframework.stereotype.Service;
@@ -23,6 +27,9 @@ public class TodoService {
   3.map(...) で DTO へ変換
   4.toList() で List に戻す
  */
+// postgre ins start
+@Transactional(readOnly = true)
+// postgre ins end
     public List<TodoResponse> findAll() {
         return todoRepository.findAll()
                 .stream()
@@ -48,12 +55,18 @@ public class TodoService {
 //    }
 //    return responses;
  */
+// postgre ins start
+@Transactional
+// postgre ins end
     public void createSampleTodosIfEmpty() {
         if (todoRepository.count() == 0) {
             todoRepository.save(new Todo("H2に保存されたTODO", false));
             todoRepository.save(new Todo("DB連携を理解する", true));
         }
     }
+    // postgre ins start
+    @Transactional
+    // postgre ins end
     public TodoResponse create(TodoCreateRequest request) {
 
         Todo todo = new Todo(
@@ -69,16 +82,33 @@ public class TodoService {
                 saved.isCompleted()
         );
     }
-    public Todo update(Long id, Todo request) {
+   // postgre upd start
+//    public Todo update(Long id, Todo request) {
+//
+//        Todo todo = todoRepository.findById(id)
+//                .orElseThrow(() -> new ResourceNotFoundException("Todo not found"));
+//
+//        todo.setTitle(request.getTitle());
+//        todo.setCompleted(request.isCompleted());
+//
+//        return todoRepository.save(todo);
+//    }
+@Transactional
+public TodoResponse update(Long id, TodoUpdateRequest request) {
+    Todo todo = todoRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Todo not found"));
 
-        Todo todo = todoRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Todo not found"));
+    todo.setTitle(request.getTitle());
+    todo.setCompleted(request.isCompleted());
 
-        todo.setTitle(request.getTitle());
-        todo.setCompleted(request.isCompleted());
+    Todo saved = todoRepository.save(todo);
 
-        return todoRepository.save(todo);
-    }
+    return new TodoResponse(saved.getId(), saved.getTitle(), saved.isCompleted());
+}
+    // postgre upd end
+    // postgre ins start
+    @Transactional
+    // postgre ins end
     public void delete(Long id) {
 
         Todo todo = todoRepository.findById(id)
